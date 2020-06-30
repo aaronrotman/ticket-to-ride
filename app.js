321652d29de4b985321d44c9ccddd9ba7e0431a9
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-06-30 16:18:01 -0700

    Add 'clear saved scores' and 'save score' buttons and added functionality to them

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // Variable to store the players score
 var score = 0;
+
 // Variable to store the points gained per click
 var pointsGained;
+
 //Variable to store the number of clicks each button received
 var numClicks = {};
 
 // Select all buttons and store the selection as a variable
 var pointButtons = d3.selectAll(".point-button");
+
 // Select the reset button and store the selection as a variable
 var resetButton = d3.select(".reset-button");
 
+// Select the score button and store the selection as a variable
+var scoreButton = d3.select(".score-button");
+
 // Function to run on page load
 function init() {
-    console.log(`Starting Score: ${score}`);
+    console.log(`Welcome! Starting Score: ${score}`);
 };
 // Event listener to handle clicks for the reset button
 resetButton.on("click", function() {
@@ -25,9 +31,10 @@ pointButtons.on("click", function() {
     // Store the id of the selected button
     var whichButton = this.id;
     // Log the selected button's id to the console
-    console.log(`You clicked a button: ${whichButton}`);
+    // console.log(`You clicked a button: ${whichButton}`);
+
     // Increase the player's score by the appropriate amount depending on the button selected
-    /* 
+    /* ---------------
     Points per button:
     buttonOne: 1
     buttonTwo: 2
@@ -35,7 +42,7 @@ pointButtons.on("click", function() {
     buttonFour: 7
     buttonFive: 10
     buttonSix: 15
-    */
+    --------------- */ 
 
     switch (whichButton) {
         case "buttonOne":
@@ -63,11 +70,29 @@ pointButtons.on("click", function() {
             score += pointsGained;
             break;
     }
-    console.log(`Value added: ${pointsGained} New Score: ${score}`)
+    console.log(`Train added | Points gained: ${pointsGained} | New score: ${score}`)
 });
-
-
-
+   
+scoreButton.on("click", function() {
+    var scoreZone = d3.select(".score-zone");
+    var playerCard = scoreZone.append("card").attr("class", "card");
+    var cardData = playerCard.append("h1").attr("class", "text-center");
+    cardData.text(`${score}`);
+    console.log(`Score Saved: ${score}`);
+    score=0;
+    console.log(`Current score Reset. Current Score: ${score}`);
+});
+clearButton = d3.select(".clear-html");
+clearButton.on("click", function() {
+    var scoreZone = d3.select(".score-zone");
+    scoreZone.html("");
+    score=0;
+    console.log("Saved scores reset.");
+    console.log(`Current score reset. Current score: ${score}`);
+});
 // Call the init() function
 init();
+
+// Declare the winner
 // console.log("Aaron wins!")
+
